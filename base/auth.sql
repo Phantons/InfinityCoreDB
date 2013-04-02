645469ea701cd31f0991ba9a58c43dae61b969ac
lordpsyan <realmsofwarcraft@gmail.com>
2013-04-02 12:15:40 -0500

    Updated structure for 5.2.0.16769. Designed to work with github.com/infinitycore. Base database only. Not populated. For testing purposes only.

---
+++ base/auth.sql
@@ -0,0 +1,166 @@
+/*Table structure for table `account` */
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Identifier',
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `sha_pass_hash` varchar(40) NOT NULL DEFAULT '',
+  `sha_email_pass_hash` varchar(40) NOT NULL DEFAULT '',
+  `sessionkey` longtext,
+  `v` longtext,
+  `s` longtext,
+  `gmlevel` int(10) NOT NULL DEFAULT '0',
+  `email` text,
+  `joindate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_ip` varchar(30) NOT NULL DEFAULT '127.0.0.1',
+  `failed_logins` int(11) unsigned NOT NULL DEFAULT '0',
+  `locked` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `online` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `active_realm_id` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `expansion` tinyint(3) unsigned NOT NULL DEFAULT '2',
+  `mutetime` bigint(40) NOT NULL DEFAULT '0',
+  `locale` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `os` varchar(4) NOT NULL DEFAULT '',
+  `recruiter` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `last_build` mediumint(5) unsigned NOT NULL DEFAULT '0',
+  `acp` int(32) NOT NULL DEFAULT '0',
+  `staff_id` int(32) NOT NULL DEFAULT '0',
+  `vp` int(10) unsigned NOT NULL DEFAULT '0',
+  `dp` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=5184 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Account System';
+
+/*Table structure for table `account_access` */
+
+DROP TABLE IF EXISTS `account_access`;
+
+CREATE TABLE `account_access` (
+  `id` int(10) unsigned NOT NULL,
+  `gmlevel` tinyint(3) unsigned NOT NULL,
+  `RealmID` int(11) NOT NULL DEFAULT '-1',
+  PRIMARY KEY (`id`,`RealmID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `account_banned` */
+
+DROP TABLE IF EXISTS `account_banned`;
+
+CREATE TABLE `account_banned` (
+  `id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Account id',
+  `bandate` int(10) unsigned NOT NULL DEFAULT '0',
+  `unbandate` int(10) unsigned NOT NULL DEFAULT '0',
+  `bannedby` varchar(50) NOT NULL,
+  `banreason` varchar(255) NOT NULL,
+  `active` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`,`bandate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Ban List';
+
+/*Table structure for table `account_friends` */
+
+DROP TABLE IF EXISTS `account_friends`;
+
+CREATE TABLE `account_friends` (
+  `id` int(11) unsigned NOT NULL DEFAULT '0',
+  `friend_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `bind_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Bring Date',
+  `expire_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Expire Date',
+  PRIMARY KEY (`id`,`friend_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 PACK_KEYS=0 COMMENT='Stores accounts for Refer-a-Friend System.';
+
+/*Table structure for table `ip_banned` */
+
+DROP TABLE IF EXISTS `ip_banned`;
+
+CREATE TABLE `ip_banned` (
+  `ip` varchar(15) NOT NULL DEFAULT '127.0.0.1',
+  `bandate` int(10) unsigned NOT NULL,
+  `unbandate` int(10) unsigned NOT NULL,
+  `bannedby` varchar(50) NOT NULL DEFAULT '[Console]',
+  `banreason` varchar(255) NOT NULL DEFAULT 'no reason',
+  PRIMARY KEY (`ip`,`bandate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Banned IPs';
+
+/*Table structure for table `logs` */
+
+DROP TABLE IF EXISTS `logs`;
+
+CREATE TABLE `logs` (
+  `time` int(10) unsigned NOT NULL,
+  `realm` int(10) unsigned NOT NULL,
+  `type` tinyint(3) unsigned NOT NULL,
+  `level` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `string` text CHARACTER SET latin1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `realm_classes` */
+
+DROP TABLE IF EXISTS `realm_classes`;
+
+CREATE TABLE `realm_classes` (
+  `realmId` int(11) NOT NULL,
+  `class` tinyint(4) NOT NULL COMMENT 'Class Id',
+  `expansion` tinyint(4) NOT NULL COMMENT 'Expansion for class activation',
+  PRIMARY KEY (`realmId`,`class`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `realm_races` */
+
+DROP TABLE IF EXISTS `realm_races`;
+
+CREATE TABLE `realm_races` (
+  `realmId` int(11) NOT NULL,
+  `race` tinyint(4) NOT NULL COMMENT 'Race Id',
+  `expansion` tinyint(4) NOT NULL COMMENT 'Expansion for race activation',
+  PRIMARY KEY (`realmId`,`race`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `realmcharacters` */
+
+DROP TABLE IF EXISTS `realmcharacters`;
+
+CREATE TABLE `realmcharacters` (
+  `realmid` int(10) unsigned NOT NULL DEFAULT '0',
+  `acctid` int(10) unsigned NOT NULL,
+  `numchars` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`realmid`,`acctid`),
+  KEY `acctid` (`acctid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Realm Character Tracker';
+
+/*Table structure for table `realmlist` */
+
+DROP TABLE IF EXISTS `realmlist`;
+
+CREATE TABLE `realmlist` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '127.0.0.1',
+  `port` smallint(5) unsigned NOT NULL DEFAULT '8085',
+  `icon` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `color` tinyint(3) unsigned NOT NULL DEFAULT '2',
+  `flag` tinyint(3) unsigned NOT NULL DEFAULT '32',
+  `realmflags` tinyint(3) unsigned NOT NULL DEFAULT '32',
+  `timezone` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `allowedSecurityLevel` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `population` float unsigned NOT NULL DEFAULT '0',
+  `realmbuilds` int(10) unsigned NOT NULL DEFAULT '12340',
+  `gamebuild` int(10) unsigned NOT NULL DEFAULT '12340',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='Realm System';
+
+/*Table structure for table `uptime` */
+
+DROP TABLE IF EXISTS `uptime`;
+
+CREATE TABLE `uptime` (
+  `realmid` int(10) unsigned NOT NULL,
+  `starttime` int(10) unsigned NOT NULL DEFAULT '0',
+  `startstring` varchar(64) NOT NULL DEFAULT '',
+  `uptime` int(10) unsigned NOT NULL DEFAULT '0',
+  `maxplayers` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `revision` varchar(255) NOT NULL DEFAULT 'NA',
+  PRIMARY KEY (`realmid`,`starttime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Uptime system';
