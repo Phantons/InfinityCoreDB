d4054bfd13c1c7fba132ab5334f5e4f403e77832
lordpsyan <realmsofwarcraft@gmail.com>
2013-04-02 23:53:34 -0500

    Original data for realm classes and races was using realmID9. CHanged it to 1.

--- base/auth.sql
+++ base/auth.sql
@@ -212,7 +212,7 @@ CREATE TABLE `realm_classes` (
   PRIMARY KEY (`realmId`,`class`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-insert  into `realm_classes`(`realmId`,`class`,`expansion`) values (9,1,0),(9,2,0),(9,3,0),(9,4,0),(9,5,0),(9,6,2),(9,7,0),(9,8,0),(9,9,0),(9,10,4),(9,11,0);
+insert  into `realm_classes`(`realmId`,`class`,`expansion`) values (1,1,0),(1,2,0),(1,3,0),(1,4,0),(1,5,0),(1,6,2),(1,7,0),(1,8,0),(1,9,0),(1,10,4),(1,11,0);
 
 /*Table structure for table `realm_races` */
 
@@ -225,7 +225,7 @@ CREATE TABLE `realm_races` (
   PRIMARY KEY (`realmId`,`race`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-insert  into `realm_races`(`realmId`,`race`,`expansion`) values (9,1,0),(9,2,0),(9,3,0),(9,4,0),(9,5,0),(9,6,0),(9,7,0),(9,8,0),(9,9,3),(9,10,1),(9,11,1),(9,22,3),(9,24,4),(9,25,4),(9,26,4);
+insert  into `realm_races`(`realmId`,`race`,`expansion`) values (1,1,0),(1,2,0),(1,3,0),(1,4,0),(1,5,0),(1,6,0),(1,7,0),(1,8,0),(1,9,3),(1,10,1),(1,11,1),(1,22,3),(1,24,4),(1,25,4),(1,26,4);
 
 /*Table structure for table `realmcharacters` */
 
